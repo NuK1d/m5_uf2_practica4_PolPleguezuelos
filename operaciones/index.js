99ef5df71b7e70495dad4667879031e0ba540375
WolfXLP <05.luque.x@gmail.com>
2025-01-23 16:54:49 +0100

    SUMA I MULTIPLICACIO CORRECTE

--- operaciones/index.js
+++ operaciones/index.js
@@ -5,19 +5,26 @@
  * @returns {Number} el resultat de la operacio
  */
 function suma(...valors) {
-
+    let resultat = 0;
+    for (let valor of valors) {
+        resultat += valor;
+    }
+    return resultat;
 }
-
 
 /**
  * Aquesta funcio multiplica tots els valors passats per parametre
  * 
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
-*/
-function multiplica(...valors) { let resultat = 1; for (let valor of
-    valors) { resultat *= valor; } 
+ */
+function multiplica(...valors) { 
+    let resultat = 1; 
+    for (let valor of valors) { 
+        resultat *= valor; 
+    }
+    return resultat; 
 }
-return resultat; 
+
 module.exports.suma = suma;
 module.exports.multiplica = multiplica;
